7da605872e6e9e1bab77fd80ae43560345d460cf
Davido91390 <david.kabore@hotmail.fr>
2024-06-28 14:05:29 +0200

    Carrousel and CSS modification

--- client/src/components/Carrousel.jsx
+++ client/src/components/Carrousel.jsx
@@ -35,22 +35,31 @@ function Carrousel() {
   }, []);
 
   return (
-    <div ref={sliderRef} className="keen-slider">
-      {movies.map((movie, index) => (
-        <div
-          key={movie.id}
-          className={`keen-slider__slide number-slide${index}`}
-        >
-          {movie.title} <br />
-          {movie.vote_average}
-        </div>
-      ))}
-      <div className="keen-slider__slide number-slide2">2</div>
+    <>
+      <h1>Tendances</h1>
+      <div ref={sliderRef} className="keen-slider">
+        {movies.map((movie, index) => (
+          <div
+            key={movie.id}
+            className={`keen-slider__slide number-slide${index}`}
+            id="film"
+          >
+            <img
+              className="posterCarrouselPicture"
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+            />
+            {movie.title} <br />
+            {movie.vote_average}
+          </div>
+        ))}
+        {/* <div className="keen-slider__slide number-slide2">2</div>
       <div className="keen-slider__slide number-slide3">3</div>
       <div className="keen-slider__slide number-slide4">4</div>
       <div className="keen-slider__slide number-slide5">5</div>
-      <div className="keen-slider__slide number-slide6">6</div>
-    </div>
+      <div className="keen-slider__slide number-slide6">6</div> */}
+      </div>
+    </>
   );
 }
 
--- client/src/components/TrendingMovies.jsx
+++ client/src/components/TrendingMovies.jsx
@@ -7,7 +7,7 @@ function TrendingMovies() {
   function FetchTrendingMovies() {
     axios
       .get(
-        `https://api.themoviedb.org/3/trending/movie/day?api_key=${import.meta.env.VITE_MY_API_KEY}&language=fr-FR`
+        `https://api.themoviedb.org/3/trending/movie/day?api_key=${import.meta.env.VITE_API_KEY}&language=fr-FR`
       )
       .then((response) => {
         setAllTrendingMovies(response.data.results);
