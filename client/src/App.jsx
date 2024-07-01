3a909cd06e4f6b3ceb119827e9cdbdb30b3ebc40
Davido91390 <david.kabore@hotmail.fr>
2024-07-01 16:06:56 +0200

    add link to navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <NavBar />
       <main>
         <Outlet />
       </main>
-      <footer>Boutons</footer>
+      <Footer />
     </>
   );
 }
