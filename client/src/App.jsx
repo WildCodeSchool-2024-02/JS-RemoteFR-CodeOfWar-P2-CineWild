5f4fbc9fb5fde5b979d0d6abf539344cedd776b2
Davido91390 <david.kabore@hotmail.fr>
2024-07-01 15:43:29 +0200

    Add Root movie

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router-dom";
-import TrendingMovies from "./components/TrendingMovies";
 
 function App() {
   return (
     <>
       <main>
-        <TrendingMovies />
         <Outlet />
       </main>
       <footer>Boutons</footer>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
 import Home from "./pages/Home";
+import Movie from "./pages/Movie";
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/movie",
+        element: <Movie />,
       },
     ],
   },
