7f0610b4737ae4472eb169c9417882e90779a67e
BaptisteFredj <redj2304@gmail.com>
2024-07-05 10:55:56 +0200

    imported svg icons for the navbar, and created navbar.css

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,11 +1,36 @@
 import { Link } from "react-router-dom";
 
+import "../styles/navbar.css";
+
 function NavBar() {
   return (
     <div>
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/movie">Movie</Link>
+        <ul className="navbuttons">
+          <li>
+            <img
+              src="../src/assets/images/menuburger.svg"
+              className="menuburger"
+              alt="Bouton menu"
+            />
+          </li>
+          <li>
+            <Link to="/">
+              <img
+                src="../src/assets/images/logocw.svg"
+                className="logocw"
+                alt="Logo"
+              />
+            </Link>
+          </li>
+          <li>
+            <img
+              src="../src/assets/images/loupe.svg"
+              className="searchbaricon"
+              alt="Loupe"
+            />
+          </li>
+        </ul>
       </nav>
     </div>
   );
