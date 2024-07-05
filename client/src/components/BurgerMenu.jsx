f5725bc2f6133650f432a559fcfef385fa9f80d5
BaptisteFredj <redj2304@gmail.com>
2024-07-05 11:11:04 +0200

    created menuburger component

---
+++ client/src/components/BurgerMenu.jsx
@@ -0,0 +1,14 @@
+export default function BurgerMenu() {
+  return (
+    <>
+      <div className="burgermenu">
+        <div className="burger burger1" />
+        <div className="burger burger2" />
+        <div className="burger burger3" />
+      </div>
+      <div>
+        <p>eslint...</p>
+      </div>
+    </>
+  );
+}
