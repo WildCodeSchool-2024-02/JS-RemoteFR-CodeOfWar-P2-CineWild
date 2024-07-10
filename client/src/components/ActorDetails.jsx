1122f2a9b7dec2e599e14450f303b9b2561f05bf
PixelKata <guillaumebasilaire@gmail.com>
2024-07-10 17:05:07 +0200

    :construction: Add a function to convert date format, add a function to calculate age, add descriptions, and add the corresponding CSS page.

--- client/src/components/ActorDetails.jsx
+++ client/src/components/ActorDetails.jsx
@@ -1,17 +1,69 @@
 import { useLoaderData } from "react-router-dom";
+import "../styles/actorDetails.css";
 
 function ActorDetails() {
   const actorInfo = useLoaderData();
   console.info(actorInfo);
 
+  const releaseDate = () => {
+    const event = new Date(actorInfo.birthday);
+    const options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    return event.toLocaleDateString("fr-FR", options);
+  };
+
+  const calculateAge = (newBirthdayDate) => {
+    const today = new Date();
+    const birthDate = new Date(newBirthdayDate);
+
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDifference = today.getMonth() - birthDate.getMonth();
+
+    // Vérifie si le jour d'anniversaire est déjà passé cette année
+    if (
+      monthDifference < 0 ||
+      (monthDifference === 0 && today.getDate() < birthDate.getDate())
+    ) {
+      age -= 1;
+    }
+    return age;
+  };
+
   return (
-    <>
-      <h1>info sur les acteurs</h1>
+    <div className="actorContainer">
       <img
         src={`https://image.tmdb.org/t/p/w500/${actorInfo.profile_path}&language=fr-FR`}
         alt={actorInfo.name}
       />
-    </>
+      <div className="actordetails">
+        <h1 className="actorName">{actorInfo.name}</h1>
+        <ul>
+          <li>
+            <span className="blue-Font">Genre:</span>{" "}
+            {actorInfo.gender === 1 ? "Femme" : "Homme"}{" "}
+          </li>
+          <li>
+            <span className="blue-Font">Lieu de Naissanse</span>{" "}
+            {actorInfo.place_of_birth}{" "}
+          </li>
+          <li>
+            <span className="blue-Font">Date de Naissanse:</span>{" "}
+            {releaseDate(actorInfo.birthday)}{" "}
+          </li>
+          <li>
+            <span className="blue-Font">Age:</span>{" "}
+            {calculateAge(actorInfo.birthday)}{" "}
+          </li>
+        </ul>
+      </div>
+      <div className="biographie">
+        <h2> Biographie</h2>
+        <p>{actorInfo.biography}</p>
+      </div>
+    </div>
   );
 }
 
