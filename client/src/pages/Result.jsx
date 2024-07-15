990d96e1f81a8f5fd6a75f734d786dbe79e87716
ChachaMm17 <charlotte.morvant@gmail.com>
2024-07-15 21:38:27 +0200

    :rocket:all filters are functional + modification of CSS + totals of operational results according to activated filters

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,60 +1,113 @@
 import { useLoaderData } from "react-router-dom";
 
+import { useState } from "react";
+
 import MovieResult from "../components/MovieResult";
 import PersonResult from "../components/PersonResult";
+
+import "../styles/result.css";
 
 export default function Result() {
   const { searchMovies, searchPersons } = useLoaderData();
 
+  const [selection, setSelection] = useState("buttonTout");
+
+  const handleChangeSelection = (buttonName) => {
+    setSelection(buttonName);
+  };
+
+  const numberResult = () => {
+    if (selection === "buttonTout")
+      return searchMovies.length + searchPersons.length;
+    if (selection === "buttonFilm") return searchMovies.length;
+    if (selection === "buttonArtiste") return searchPersons.length;
+    return 0;
+  };
+
+  const result = numberResult();
+
   const pluralSingularResults = () =>
-    searchMovies.length + searchPersons.length < 2
-      ? "RÉSULTAT :"
-      : "RÉSULTATS :";
+    result < 2 ? "RÉSULTAT :" : "RÉSULTATS :";
 
   return (
     <section>
-      {searchMovies.length + searchPersons.length === 0 ? (
+      {result === 0 ? (
         <p>Oops ! Aucun résultat</p>
       ) : (
         <div className="sentenceResult">
           <p>{pluralSingularResults()}</p>
           <p>
-            {`${searchMovies.length}
+            {`${result}
             ${pluralSingularResults().toLowerCase().slice(0, -2)}`}
           </p>
         </div>
       )}
+
       {searchMovies.length + searchPersons.length === 0 ? null : (
-        <div className="separator">
+        <div className="separatorFilter">
           <ul>
             <li>
-              <button type="button" className="buttonTout">
+              <button
+                type="button"
+                className="buttonTout"
+                onClick={() => handleChangeSelection("buttonTout")}
+              >
                 Tout
               </button>
             </li>
             <li>
-              <button type="button" className="buttonFilm">
+              <button
+                type="button"
+                className="buttonFilm"
+                onClick={() => handleChangeSelection("buttonFilm")}
+              >
                 Film
               </button>
             </li>
             <li>
-              <button type="button" className="buttonArtiste">
+              <button
+                type="button"
+                className="buttonArtiste"
+                onClick={() => handleChangeSelection("buttonArtiste")}
+              >
                 Artiste
               </button>
             </li>
           </ul>
         </div>
       )}
-      {!searchMovies
-        ? null
-        : searchMovies.map((movie) => (
-            <MovieResult key={movie.id} movie={movie} />
-          ))}
-      {!searchPersons
-        ? null
-        : searchPersons.map((person) => (
-            <PersonResult key={person.id} person={person} />
-          ))}
+      {selection === "buttonFilm" && (
+        <div>
+          {!searchMovies
+            ? null
+            : searchMovies.map((movie) => (
+                <MovieResult key={movie.id} movie={movie} />
+              ))}
+        </div>
+      )}
+      {selection === "buttonArtiste" && (
+        <div>
+          {!searchPersons
+            ? null
+            : searchPersons.map((person) => (
+                <PersonResult key={person.id} person={person} />
+              ))}
+        </div>
+      )}
+      {selection === "buttonTout" && (
+        <div>
+          {!searchMovies
+            ? null
+            : searchMovies.map((movie) => (
+                <MovieResult key={movie.id} movie={movie} />
+              ))}
+          {!searchPersons
+            ? null
+            : searchPersons.map((person) => (
+                <PersonResult key={person.id} person={person} />
+              ))}
+        </div>
+      )}
     </section>
   );
 }
