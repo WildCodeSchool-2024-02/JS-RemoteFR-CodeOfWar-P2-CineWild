b7c36091b16541c3a332b0e37f28c2e22a6ddada
ChachaMm17 <charlotte.morvant@gmail.com>
2024-07-15 11:06:05 +0200

    :rocket: Display of a message in case of no results and condition on result spelling depending on the number of results found

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -2,29 +2,25 @@ import { useLoaderData } from "react-router-dom";
 
 import MovieResult from "../components/MovieResult";
 
-// import MovieDetails from "../components/MovieDetails";
-
 export default function Result() {
   const { searchMovies } = useLoaderData();
   console.info(searchMovies);
 
-  // const pluralSingularResults = () =>
-  //   searchMovies.length < 2 ? "RÉSULTAT" : "RÉSULTATS";
+  const pluralSingularResults = () =>
+    searchMovies.length < 2 ? "RÉSULTAT :" : "RÉSULTATS :";
 
   return (
     <section>
-      {searchMovies.map((movie) => (
-        <MovieResult key={movie.id} movie={movie} />
-      ))}
-
-      {/* {searchMovies === null
-        ? "Oops ! Aucun résultat"
-        : pluralSingularResults()}
+      {searchMovies.length === 0 ? (
+        <p className="sentenceResult">Oops ! Aucun résultat</p>
+      ) : (
+        <p className="sentenceResult">{pluralSingularResults()}</p>
+      )}
       {!searchMovies
         ? null
         : searchMovies.map((movie) => (
-            <MovieDetails key={movie.idMovie} movie={movie} />
-          ))} */}
+            <MovieResult key={movie.id} movie={movie} />
+          ))}
     </section>
   );
 }
