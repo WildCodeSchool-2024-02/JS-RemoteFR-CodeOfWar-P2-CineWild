1238726c2aa9734344151a615c46a68b58fde90c
BaptisteFredj <redj2304@gmail.com>
2024-07-16 15:17:21 +0200

    added nowplaying carrousel

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,14 +2,17 @@ import { useLoaderData } from "react-router-dom";
 import RandomMovie from "../components/RandomMovie";
 import Carrousel from "../components/Carrousel";
 import PopularMovies from "../components/PopularMovies";
+import PlayingMovies from "../components/NowPlaying";
 
 function Home() {
-  const { trendingMovies, popularMovies, randomMovie } = useLoaderData();
+  const { trendingMovies, popularMovies, randomMovie, playingMovies } =
+    useLoaderData();
   return (
     <>
       <RandomMovie randomMovie={randomMovie} />
       <Carrousel trendingMovies={trendingMovies} />
       <PopularMovies popularMovies={popularMovies} />
+      <PlayingMovies playingMovies={playingMovies} />
     </>
   );
 }
